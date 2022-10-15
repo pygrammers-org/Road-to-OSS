17bf8454f79162ef25a090bf8102c188c5855874
Shaheen Hyder <shaheenhyderk@gmail.com>
2022-10-16 01:53:25 +0530

    some js restructuring in code

--- deps/script.js
+++ deps/script.js
@@ -1,101 +1,99 @@
-const userCardTemplate = document.querySelector("[user-template]")
-const userCardContainer = document.querySelector("[user-cards-container]")
-const searchInput = document.querySelector("[user-search]")
-const temp = document.getElementById("temp")
+; (function () {
+  const userCardTemplate = document.querySelector("[user-template]");
+  const userCardContainer = document.querySelector("[user-cards-container]");
+  const searchInput = document.querySelector("[user-search]");
+  const temp = document.getElementById("temp");
 
-let users = []
+  let users = []
 
-function search(value) {
-  users.forEach(user => {
-    let isVisible = user.name.toLowerCase().includes(value.toLowerCase())
-    user.element.classList.toggle("hide", !isVisible)
-  })
-}
-
-searchInput.addEventListener("input", e => {
-  search(e.target.value)
-})
-
-fetch("info.json")
-  .then(res => res.json())
-  .then(data => {
-    users = data.map(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0]
-      const image = card.querySelector("[user-image]")
-      const name = card.querySelector("[user-name]")
-      const username = card.querySelector("[user-username]")
-      const userUrl = card.querySelector("[user-url]")
-      const place = card.querySelector("[user-place]")
-      const position = card.querySelector("[user-position]")
-      const bio = card.querySelector("[user-bio]")
-      let isImageAvailable = user.image == null || user.image == "null" || user.image == ""
-      image.src = isImageAvailable ? 'images/johndoe.png' : "images/" + user.image
-      name.textContent = user.name
-      userUrl.href = "https://github.com/" + user.gh_username
-      username.textContent = user.gh_username
-      place.textContent = user.place
-      position.textContent = user.current_pos
-      bio.textContent = user.bio
-      userCardContainer.append(card)
-      return {
-        name: user.name,
-        element: card
+  function search(value) {
+    users.forEach(user => {
+      if (value) {
+        let isVisible = user.name.toLowerCase().includes(value.toLowerCase());
+        user.element.classList.toggle("hide", !isVisible);
       }
     })
-    addDownloadEventListener()
-    loadSearchFromUrl()
+  }
+
+  searchInput.addEventListener("input", e => {
+    search(e.target.value);
   })
 
-function addDownloadEventListener() {
-  document.querySelectorAll('.profile-download')
-    .forEach(el => el.addEventListener("click", function () {
-      let card = this.parentElement.cloneNode(true)
-      card.children.item(6).remove();
+  fetch("info.json")
+    .then(res => res.json())
+    .then(data => {
+      users = data.map(user => {
+        const card = userCardTemplate.content.cloneNode(true).children[0];
+        let isImageAvailable = user.image == null || user.image == "null" || user.image == "";
+        card.querySelector("[user-image]").src = isImageAvailable ? 'images/johndoe.png' : "images/" + user.image;
+        card.querySelector("[user-name]").textContent = user.name;
+        card.querySelector("[user-username]").textContent = user.gh_username;
+        card.querySelector("[user-url]").href = "https://github.com/" + user.gh_username;
+        card.querySelector("[user-place]").textContent = user.place;
+        card.querySelector("[user-position]").textContent = user.current_pos;
+        card.querySelector("[user-bio]").textContent = user.bio;
+        userCardContainer.append(card);
+        return {
+          name: user.name,
+          element: card
+        }
+      })
+      addDownloadEventListener();
+      loadSearchFromUrl();
+    })
 
-      let logos = document.createElement("div");
-      logos.className = "logos"
-      let pLogo = document.createElement("img");
-      pLogo.className = "pLogo"
-      pLogo.src = "deps/py-l.png"
-      let hfLogo = document.createElement("img");
-      hfLogo.className = "hfLogo"
-      hfLogo.src = "deps/hf-l.png"
+  function addDownloadEventListener() {
+    document.querySelectorAll('.profile-download')
+      .forEach(el => el.addEventListener("click", function () {
+        let card = this.parentElement.cloneNode(true)
+        card.children.item(6).remove();
 
-      logos.appendChild(pLogo)
-      logos.appendChild(hfLogo)
+        let logos = document.createElement("div");
+        logos.className = "logos"
+        let pLogo = document.createElement("img");
+        pLogo.className = "pLogo"
+        pLogo.src = "deps/py-l.png"
+        let hfLogo = document.createElement("img");
+        hfLogo.className = "hfLogo"
+        hfLogo.src = "deps/hf-l.png"
 
-      let footer = document.createElement("div");
-      footer.className = "footer";
-      let p = document.createElement("p");
-      p.textContent = "pygrammers.org/Road-To-OSS"
-      footer.appendChild(p);
+        logos.appendChild(pLogo)
+        logos.appendChild(hfLogo)
 
-      let cardWrapper = document.createElement("div");
-      cardWrapper.className = "parent-card"
-      cardWrapper.appendChild(logos)
-      cardWrapper.appendChild(card)
-      cardWrapper.appendChild(footer)
-      temp.appendChild(cardWrapper)
+        let footer = document.createElement("div");
+        footer.className = "footer";
+        let p = document.createElement("p");
+        p.textContent = "pygrammers.org/Road-To-OSS"
+        footer.appendChild(p);
 
-      html2canvas(cardWrapper).then(function (canvas) {
-        var anchorTag = document.createElement("a");
-        document.body.appendChild(anchorTag);
-        anchorTag.download = "profile.png";
-        anchorTag.href = canvas.toDataURL();
-        anchorTag.target = '_blank';
-        anchorTag.click();
-      });
+        let cardWrapper = document.createElement("div");
+        cardWrapper.className = "parent-card"
+        cardWrapper.appendChild(logos)
+        cardWrapper.appendChild(card)
+        cardWrapper.appendChild(footer)
+        temp.appendChild(cardWrapper)
 
-      cardWrapper.remove()
-    }));
-}
+        html2canvas(cardWrapper).then(function (canvas) {
+          var anchorTag = document.createElement("a");
+          document.body.appendChild(anchorTag);
+          anchorTag.download = "profile.png";
+          anchorTag.href = canvas.toDataURL();
+          anchorTag.target = '_blank';
+          anchorTag.click();
+        });
 
-function loadSearchFromUrl() {
-  // Fetch query string from url
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
-  let value = params.search;
-  searchInput.value = value;
-  search(value);
-}
+        cardWrapper.remove();
+      }));
+  }
+
+  function loadSearchFromUrl() {
+    // Fetch query string from url
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+      get: (searchParams, prop) => searchParams.get(prop),
+    });
+    let value = params.search;
+    searchInput.value = value;
+    search(value);
+  }
+
+}());
